eec63d39feebbcd8d0e2f5a501823648981a172f
Arrchana Pradeepan <arrchana.pradeepan@gmail.com>
2016-07-16 19:26:11 -0400

    Initial commit of number guessing game

---
+++ guess-the-number.py
@@ -0,0 +1,13 @@
+#Guess what number the random number generator generated between 1 ane 100
+
+import random
+
+number = random.randint(1,10)
+guess = 0
+while guess != number:
+    guess = int(input("Take a guess: "))
+    if guess > number:
+        print("Too high!")
+    elif guess < number:
+        print("Too low!")
+print("Right guess!")
