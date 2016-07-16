ec1823376e5160442a2e80cc1e9217be335d77c7
Arrchana Pradeepan <arrchana.pradeepan@gmail.com>
2016-07-16 19:31:58 -0400

    Clearer instruction message to user

--- guess-the-number.py
+++ guess-the-number.py
@@ -4,8 +4,9 @@ import random
 
 number = random.randint(1,10)
 guess = 0
+
 while guess != number:
-    guess = int(input("Take a guess: "))
+    guess = int(input("Guess Mr.CLI's number between 1 and 10: "))
     if guess > number:
         print("Too high!")
     elif guess < number:
