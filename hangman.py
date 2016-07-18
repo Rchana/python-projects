ba316fa1cb1b5f178d31f1b0853d3154cf0c7034
Arrchana Pradeepan <arrchana.pradeepan@gmail.com>
2016-07-17 21:25:52 -0400

    Smarter hangman tracking system with additional play again option, & initial commit of tic tac toe game

--- hangman.py
+++ hangman.py
@@ -1,26 +1,40 @@
-#
+#Hangman Game
 import random
 
 print("")
 print("~ WELCOME TO HANGMAN ~")
 print("")
 
-word_list = ["school", "flowers", "Canada", "movies", "Food"]
-word = str(random.choice(word_list))
-length = len(word)
-lives = length + 3
-correct_guesses = 0
-list_of_letters = list(word)
+yes_no = "yes"
 
-print("A word has been selected. The word has", length, "letters and you have ", lives, "guesses.")
+while yes_no == "yes":
+    word_list = ["school", "flowers", "Canada", "movies", "Food"]
+    word = str(random.choice(word_list)).lower()
+    length = len(word)
+    lives = length + 3
+    correct_guesses = 0
+    list_of_letters = list(word)
 
-while lives > 0:
-    letter = str(input("Enter a guess: "))
-    lives = lives -1
-    if letter in list_of_letters:
-        correct_guesses = correct_guesses +1
-        list_of_letters.remove(letter)
-        print("Correct guess!", letter, "is in the word!")
-        if correct_guesses == length:
-            print("You win! You guessed <", word, "> correctly!!")
-print("You've ran out of lives. The word was:", word)
+    print("")
+    print("A word has been selected. The word has", length, "letters and you have", lives, "guesses.")
+    print("")
+    while lives > 0:
+        print("")
+        print("You have", lives, "lives left.")
+        letter = str(input("Enter a guess: "))
+        lives = lives -1
+        if letter in list_of_letters:
+            correct_guesses = correct_guesses +1
+            while letter in list_of_letters:
+                list_of_letters.remove(letter)
+            print("Correct guess!", letter, "is in the word!")
+            if len(list_of_letters) == 0:
+                print("")
+                print("You win! You guessed <", word, "> correctly!!")
+                break
+            elif lives == 0 and len(list_of_letters) != 0:
+                print("You've ran out of lives. The word was:", word)
+        elif lives == 0 and len(list_of_letters) != 0:
+            print("You've ran out of lives. The word was:", word)
+    print("")
+    yes_no = str(input("Do you want to play again? ")).lower()
---
+++ tictactoe.py
@@ -0,0 +1,5 @@
+#Tic Tac Toe game
+
+print("|| TIC TAC TOE ||")
+print("")
+echo ("Presss Enter to start game...")
